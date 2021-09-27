87b5f875b7ed10ded957cccc239c4bd1a810772f
Marcos <marcos.caramalho@gmail.com>
2021-09-27 23:56:27 +0100

    Updates intall.ps1 file with chocolatey's method of installing fonts

--- install.ps1
+++ install.ps1
@@ -1,21 +1,5 @@
 #Requires -Version 3.0
-<#
-.SYNOPSIS
-    Installs the provided fonts.
-.DESCRIPTION
-    Installs all the provided fonts by default.  The FontName
-    parameter can be used to pick a subset of fonts to install.
-.EXAMPLE
-    C:\PS> ./install.ps1
-    Installs all the fonts located in the Git repository.
-.EXAMPLE
-    C:\PS> ./install.ps1 FiraCode, Hack
-    Installs all the FiraCode and Hack fonts.
-.EXAMPLE
-    C:\PS> ./install.ps1 DejaVuSansMono -WhatIf
-    Shows which fonts would be installed without actually installing the fonts.
-    Remove the "-WhatIf" to install the fonts.
-#>
+
 [CmdletBinding(SupportsShouldProcess)]
 param ()
 
@@ -29,7 +13,7 @@ dynamicparam {
     [string[]]$FontNames = Join-Path $PSScriptRoot patched-fonts | Get-ChildItem -Directory -Name
     $Attributes.Add([ValidateSet]::new(($FontNames)))
 
-    $Parameter = [Management.Automation.RuntimeDefinedParameter]::new('FontName',  [string[]], $Attributes)
+    $Parameter = [Management.Automation.RuntimeDefinedParameter]::new('FontName', [string[]], $Attributes)
     $RuntimeParams = [Management.Automation.RuntimeDefinedParameterDictionary]::new()
     $RuntimeParams.Add('FontName', $Parameter)
 
@@ -37,26 +21,92 @@ dynamicparam {
 }
 
 end {
+    Function New-FontResourceType {
+        $fontCSharpCode = @'
+            using System;
+            using System.Collections.Generic;
+            using System.Text;
+            using System.IO;
+            using System.Runtime.InteropServices;
+    
+            namespace FontResource {
+                public class AddFonts {
+                    private static IntPtr HWND_BROADCAST = new IntPtr(0xffff);
+    
+                    [DllImport("gdi32.dll")]
+                    static extern int AddFontResource(string lpFilename);
+    
+                    [DllImport("gdi32.dll")]
+                    static extern int RemoveFontResource(string lpFileName);
+    
+                    [return: MarshalAs(UnmanagedType.Bool)]
+                    [DllImport("user32.dll", SetLastError = true)]
+                    private static extern bool PostMessage(IntPtr hWnd, WM Msg, IntPtr wParam, IntPtr lParam);
+    
+                    public enum WM : uint {
+                        FONTCHANGE = 0x001D
+                    }
+                
+                    public static int AddFont(string fontFilePath) {
+                        FileInfo fontFile = new FileInfo(fontFilePath);
+                        if (!fontFile.Exists) { throw new FileNotFoundException("Font file not found"); }
+                        try {
+                            int retVal = AddFontResource(fontFilePath);
+                            bool posted = PostMessage(HWND_BROADCAST, WM.FONTCHANGE, IntPtr.Zero, IntPtr.Zero);
+                            return retVal;
+                        }
+                        catch { throw new System.ComponentModel.Win32Exception(Marshal.GetLastWin32Error()); }
+                    }
+                }
+            }
+'@
+
+        Add-Type $fontCSharpCode
+    }
+
     $FontName = $PSBoundParameters.FontName
-    if (-not $FontName) {$FontName = '*'}
+    if (-not $FontName) { $FontName = '*' }
 
     $fontFiles = [Collections.Generic.List[System.IO.FileInfo]]::new()
 
     Join-Path $PSScriptRoot patched-fonts | Push-Location
     foreach ($aFontName in $FontName) {
-        Get-ChildItem $aFontName -Filter "*.ttf" -Recurse | Foreach-Object {$fontFiles.Add($_)}
-        Get-ChildItem $aFontName -Filter "*.otf" -Recurse | Foreach-Object {$fontFiles.Add($_)}
+        Get-ChildItem $aFontName -Filter "*.ttf" -Recurse | Foreach-Object { $fontFiles.Add($_) }
+        Get-ChildItem $aFontName -Filter "*.otf" -Recurse | Foreach-Object { $fontFiles.Add($_) }
     }
     Pop-Location
 
-    $fonts = $null
+    try { $LoadedType = [FontResource.AddFonts].ispublic } catch { $LoadedType = $false }
+    if (-not $LoadedType) { New-FontResourceType }
+
+    $fontRegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts'
+    $fontFileTypes = @{
+        '.fon' = ''
+        '.fnt' = ''
+        '.ttf' = ' (TrueType)'
+        '.ttc' = ' (TrueType)'
+        '.otf' = ' (OpenType)'
+    }
+
+    $shellApp = New-Object -ComObject shell.application
+    $fonts = $shellApp.NameSpace(0x14)
+    $fontsFolder = $fonts.self.path
     foreach ($fontFile in $fontFiles) {
-        if ($PSCmdlet.ShouldProcess($fontFile.Name, "Install Font")) {
-            if (!$fonts) {
-                $shellApp = New-Object -ComObject shell.application
-                $fonts = $shellApp.NameSpace(0x14)
-            }
-            $fonts.CopyHere($fontFile.FullName)
+        $folderObj = (New-Object -ComObject shell.application).namespace($fontFile.directoryname)
+        $fileObj = $folderObj.Items().Item($fontFile.Name)
+        $fontName = $folderObj.GetDetailsOf($fileObj, 21)
+
+        Copy-Item $fontFile.FullName -Destination $fontsFolder -Force
+
+        $fontFinalPath = Join-Path $fontsFolder $fontFile.Name
+        $retVal = [FontResource.AddRemoveFonts]::AddFont($fontFinalPath)
+
+        if ($retVal -eq 0) {
+            Write-Host "Font resource, '$($fontFile.FullName)', installation failed"
+        }
+        else {
+            Write-Host "Font resource, '$($fontFile.Name)', installed successfully" -ForegroundColor Green
+            Set-ItemProperty -Path "$($fontRegistryPath)" -Name "$fontName$($fontFileTypes.item($fontFile.Extension))" -Value "$($fontFile.Name)" -Type STRING -Force
         }
     }
 }
